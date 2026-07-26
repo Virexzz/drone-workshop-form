d987292bfd54345df3c1f1c8386ef05683651a9e
Virexzz <pratapgautam4321@gmail.com>
2026-07-26 22:06:23 +0545

    Modify Backend 2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+
 
 const app = express();
 
